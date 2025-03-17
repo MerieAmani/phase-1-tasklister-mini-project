d432b3b00d9c9b69d93e6dc0f7e1b0cd0e9295ff
matthew <meriematthew01@gmail.com>
2025-03-17 16:10:20 +0300

    adds tasklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  //Creating a variable by taking ID for the form and tasklist
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  //submiting the form
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();//prevent page from refreshing
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    if (taskDescription) {
+      const taskItem = document.createElement("li");//cretas list where the inputed tasks will be seen
+      taskItem.textContent = taskDescription;//task inputed
+
+      //delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => {
+        taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);//makes delete button appear nexxt to task item
+      taskList.appendChild(taskItem);//adds the above to the taskItem
+
+      form.reset();
+    }
+  });
 });
